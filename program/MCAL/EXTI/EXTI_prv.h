cf0ccd6df4f3728a3f65a4f14211351980bc1477
elwazanmoamen95 <elwazanmoamen@gmail.com>
2026-05-16 17:24:38 +0300

    branch exti from gpio

---
+++ program/MCAL/EXTI/EXTI_prv.h
@@ -0,0 +1,17 @@
+#ifndef EXTI_PRV_H
+#define EXTI_PRV_H
+
+#include "../../lib/STD_TYPES.h"
+#include "../../LIB/STM32F401xC_HEADER.h"
+
+typedef struct
+{
+   volatile u32 
+    
+}EXTI_TYPEDEF;
+
+
+#define EXTI_STRUCT     ((EXTI_TYPEDEF*)(EXTI_BASE_ADDR))
+
+
+#endif
